12810b29ad71d89c549026e13c98a6b6ce683633
shawn <thankhandsome@gmail.com>
2017-09-05 00:33:41 +0800

    first commit

---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+static bool bToprint = true;
+void myprint(){
+	cout<<endl;
+}
+
+template<typename Type, typename... Types>
+void myprint(const Type& arg, const Types&... args)
+{
+	if(bToprint == true){
+		cout<<arg<<" ";
+		myprint(args...);
+	}
+}
+int main()
+{
+	bToprint = true;
+	cout<<"hello, world!"<<endl;
+	myprint("shuaiguo", 12.5);
+	return 0;
+}
